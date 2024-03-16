2fa9767f78b7182f571523c351b27ec9af0bf515
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2024-03-16 18:38:24 +0530

    testing with a name

--- basics/hello_test.go
+++ basics/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello World"
+	got := Hello("Gupta")
+	want := "Hello Gupta"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
